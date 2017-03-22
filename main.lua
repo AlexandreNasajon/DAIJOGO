949c4f3e7ffa619c37c3f36ba19d7128b76f813a
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-03-21 23:24:52 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,17 @@
 --ignora-----
 math.randomseed(os.time())
 -------------
-
+function printcarta(carta)
+  print(carta["name"],carta["atq"],carta["def"])
+end
+function printmao(hand)
+  i = 1
+  while i <= #hand do
+    printcarta(hand[i])
+  
+    i = i+1
+  end  
+end
 card = {
   {["name"] = "mago",["atq"]=25,["def"]=21},
   {["name"] = "demon",["atq"]=20,["def"]=15},
@@ -11,24 +21,34 @@ card = {
 }
 
 deck = {card[1],card[2],card[3],card[4],card[5]}
+mao = {deck[math.random(1,5)],deck[math.random(1,5)],deck[math.random(1,5)]}
 
 --numerocartasmao = 3
 texto1="sua vez! Sua mão é:"
 texto2="digite o número da carta."
 --numrand = math.random(1,5)
-mao = {deck[math.random(1,5)],deck[math.random(1,5)],deck[math.random(1,5)]}
-print(texto1)
-print(mao[1]["name"],mao[1]["atq"],mao[1]["def"])
-print(mao[2]["name"],mao[2]["atq"],mao[2]["def"])
-print(mao[3]["name"],mao[3]["atq"],mao[3]["def"])
-print(texto2)
 
-num = tonumber(io.read())
+while #mao>0 do
+  print(texto1)
+  printmao(mao)
+  print(texto2)
 
-t={}
-valido = num<=3 and num>0
-if valido then
-  print(t[num][1],t[num][2],t[num][3])
-else 
-  print("Este numero nao eh valido!")
+  num = tonumber(io.read())
+
+  valido = num<=3 and num>0
+  if valido then
+    printcarta(mao[num])
+    mao[num]=nil
+    
+    n = num
+    while n <= #mao do
+      mao[n] = mao[n+1]
+      n = n+1
+    end
+      
+    print("Sua nova mao eh:")
+    printmao(mao)
+  else 
+    print("Este numero nao eh valido!")
+  end
 end
