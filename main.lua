3faf7fe6ee53859b6da2d03979e378feb6150cec
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-03-23 15:15:19 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,23 +1,22 @@
-
 math.randomseed(os.time())
 
 function printcard(c)
   print(">",c["type"],c["name"],c["power"],c["resistance"])
 end
 function printhand(h)
-  i = 1
-  while i <= #h do
-    printcard(h[i])
+    i = 1
+    while i <= #h do
+        printcard(h[i])
   
-    i = i+1
-  end  
+        i = i+1
+    end  
 end
 function printboard(b)
     m = 1
     while m <= #b do
-      printcard(b[m])
+        printcard(b[m])
    
-      m = m+1
+        m = m+1
     end
 end
 
@@ -28,11 +27,13 @@ card = {
   {["type"] = "monster",["name"] = "Witch",["power"] = 30,["resistance"]=15},
   {["type"] = "monster",["name"] = "Wolf",["power"] = 15,["resistance"]=15},
   {["type"] = "monster",["name"] = "Tiger",["power"] = 20,["resistance"]=15},
-  {["type"] = "monster",["name"] = "Angel",["power"] = 25,["resistance"]=25}
+  {["type"] = "monster",["name"] = "Angel",["power"] = 25,["resistance"]=25},
+  {["type"] = "monster",["name"] = "Dragon",["power"] = 40,["resistance"]=20},
+  {["type"] = "monster",["name"] = "Kraken",["power"] = 35,["resistance"]=30}
 }
 
-deck = {card[1],card[2],card[3],card[4],card[5],card[6],card[7]}
-hand = {deck[math.random(1,#card)],deck[math.random(1,#card)],deck[math.random(1,#card)]}
+deck = {card[1],card[2],card[3],card[4],card[5],card[6],card[7],card[8],card[9]}
+hand = {deck[math.random(1,#deck)],deck[math.random(1,#deck)],deck[math.random(1,#deck)]}
 board = {}
 
 while #hand>0 do
@@ -49,12 +50,10 @@ while #hand>0 do
     board[#board+1] = hand[num]
     hand[num] = nil
     
-    n = num
-    while n <= #hand do
-      hand[n] = hand[n+1]
-      n = n+1
+    while num <= #hand do
+      hand[num] = hand[num+1]
+      num = num+1
     end
-      
    
     print("The board is:")
     printboard(board)
