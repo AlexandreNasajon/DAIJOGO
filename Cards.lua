6ca674853b1479ba94fa2f6187b707cdee4f93c8
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-05-17 01:27:23 -0300

    Create Cards.lua

---
+++ Cards.lua
@@ -0,0 +1,16 @@
+local Cards = {}
+
+Cards.Soldado = {
+    nome = "Soldier",
+    poder = 2,
+    custo = 1,
+    tipo = "Unidade"
+}
+Cards.Milicia = {
+    nome = "Milícia",
+    poder = 1,
+    custo = 0,
+    tipo = "Unidade"
+}
+        
+return Cards
