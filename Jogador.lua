7f736086afeba6c1febfc0e755f26a2f86eb7852
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-05-17 01:27:06 -0300

    Create Jogador.lua

---
+++ Jogador.lua
@@ -0,0 +1,22 @@
+local Jogador = {}
+
+
+Jogador[1] = {
+    vida = 10,
+    ouro = 0,
+    deck = {},
+    mao = {},
+    campo = {},
+    cemiterio = {}
+}
+
+Jogador[2] = {
+    vida = 10,
+    ouro = 0,
+    deck = {},
+    mao = {},
+    campo = {},
+    cemiterio = {}
+}
+
+return Jogador
