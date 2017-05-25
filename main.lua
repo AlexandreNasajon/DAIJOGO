2ca5c6bbffc70f18c3894813d65af2ac5c99be35
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-05-25 00:05:55 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,15 @@ math.randomseed(os.time())
 local Funcoes = require("Funcoes")
 local Jogador = require("Jogador")
 local Cards = require("Cards")
-Jogador[1].deck = {Cards.Soldado,Cards.Milicia}
+
+local cardtemp = {}
+
+Jogador[1].pool = {Cards.Elfo,Cards.Elfo}
+Jogador[1].deck = {}
+for k,v in pairs(Jogador[1].pool) do
+  cardtemp = Funcoes.copiar(v,cardtemp)
+  Jogador[1].deck[#Jogador[1].deck+1] = cardtemp
+end
+
 Jogador[2].deck = {Cards.Soldado,Cards.Milicia}
 Funcoes.jogo()
