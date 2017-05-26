d5e73ed976b6fb0aac1bded93558ab07f680aa52
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-05-25 21:47:54 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,15 +2,36 @@ math.randomseed(os.time())
 local Funcoes = require("Funcoes")
 local Jogador = require("Jogador")
 local Cards = require("Cards")
+Jogador[1].pool = {Cards.Elfo,Cards.Elfo,Cards.Elfo,Cards.Elfo,Cards.Elfo,Cards.Elfo,Cards.Elfo,Cards.Elfo,
+    Cards.Stormear,Cards.Stormear,Cards.Stormear,
+    Cards.Ganancia,Cards.Ganancia,Cards.Ganancia,
+    Cards.Bolt,Cards.Bolt,Cards.Bolt,
+    Cards.Cooperar,Cards.Cooperar,Cards.Cooperar,
+    Cards.Pote,Cards.Pote,Cards.Pote,
+    Cards.Brainstorm,
+    Cards.Firestorm
+}
 
-local cardtemp = {}
+Jogador[2].pool = {Cards.Troll,Cards.Troll,Cards.Troll,Cards.Furia,Cards.Furia,Cards.Furia}
 
-Jogador[1].pool = {Cards.Elfo,Cards.Elfo}
+
+tempcard = {}
+
 Jogador[1].deck = {}
+Jogador[2].deck = {}
+
 for k,v in pairs(Jogador[1].pool) do
-  cardtemp = Funcoes.copiar(v,cardtemp)
-  Jogador[1].deck[#Jogador[1].deck+1] = cardtemp
+    tempcard = Funcoes.copiar(v,tempcard)
+    Jogador[1].deck[#Jogador[1].deck+1] = tempcard
+    tempcard = {}
 end
 
-Jogador[2].deck = {Cards.Soldado,Cards.Milicia}
+
+for k,v in pairs(Jogador[2].pool) do
+    tempcard = Funcoes.copiar(v,tempcard)
+    Jogador[2].deck[#Jogador[2].deck+1] = tempcard
+    tempcard = {}
+end
+
+
 Funcoes.jogo()
