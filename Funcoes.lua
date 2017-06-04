6adbad24b721c5792ec79fabb1187e5982ed57d0
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-06-04 19:13:40 -0300

    Update Funcoes.lua

--- Funcoes.lua
+++ Funcoes.lua
@@ -69,7 +69,7 @@ end
 Functions.discard = function(Player1)
     local h = false
     while h == false do
-        print("Descarte um card:")
+        print("Discard a card:")
         Functions.printzone(Player1.hand)
         local opcao = tonumber(io.read())
         if opcao ~= nil and opcao <= #Player1.hand and opcao > 0 then
@@ -80,7 +80,7 @@ Functions.discard = function(Player1)
             end
             h = true
         else
-            print("VOCÊ DEVE DESCARTAR UM CARD!")
+            print("YOU MUST DISCARD A CARD!")
         end
     end
     if Player1.graveyard[#Player1.graveyard].type ~= "Support" then
@@ -93,23 +93,23 @@ end
 Functions.printzone = function(zone)
 
     local i = 1
-    print("#","name     ","cost","type","power","Lealdade")
+    print("#","Name       ","Cost","Type","Power","Loyalty")
     while i <= #zone do
-        print(i,zone[i].name,zone[i].cost,zone[i].type,zone[i].power,zone[i].lealdade)
+        print(i,zone[i].name,zone[i].cost,zone[i].type,zone[i].power,zone[i].loyalty)
         i = i+1
     end    
 end
 ----------- PRINT CARD---------
 Functions.printcard = function(card)
-    print("name: "..card.name)
-    print("cost: "..card.cost)
-    print("type: "..card.type)
+    print("Name: "..card.name)
+    print("Cost: "..card.cost)
+    print("Type: "..card.type)
     if card.power then
         print("power: "..card.power)
     end
-    print("Descrição: "..card.description)
-    if card.lealdade then
-        print("Lealdade: "..card.lealdade)
+    print("Description: "..card.description)
+    if card.loyalty then
+        print("Loyalty: "..card.loyalty)
     end
 end
 -----------DESTROY------------
@@ -192,7 +192,7 @@ end
 Functions.summon = function(card,Player1,Player2)
     if card.type == "Unit" then
         Player1.field[#Player1.field+1] = card
-        print(card.name.." foi invocado.")
+        print(card.name.." was summoned.")
         if card.effect.ifsummoned then
             card.effect.ifsummoned(Player1,Player2)
         end
@@ -231,7 +231,7 @@ end
 ----------STORM-------------rajada--------------------------------------------
 Functions.storm = function(Player1,quantidade)
     Player1.storm = Player1.storm+quantidade
-    print(Player1.name.." aumentou seu storm em "..quantidade..".")
+    print(Player1.name.."'s storm was raised by "..quantidade..".")
 end
 ---------ability-------------------------------------------------------
 Functions.ability = function(card)
@@ -239,21 +239,21 @@ Functions.ability = function(card)
         card.stamina = card.stamina-1
         card.effect()
     else 
-        print("ESTA Unit NÃO TEM MAIS ENERGIA!")
+        print("THIS UNIT HAS NO ENERGY LEFT!")
     end
 end
 -----------end turn------------nao testada
 Functions.endturn = function(Player1,Player2)
-            print("end turn de "..Player1.name..".")
+            print(Player1.name.." turn ends.")
             Player1.storm = 0
             if #Player2.deck > 0 then
                 Functions.draw(Player2)
-                print(Player2.name.." compra um card.")
+                print(Player2.name.." draws a card.")
             else
-                print(Player2.name.." não tem mais cards no deck.")
+                print(Player2.name.."'s deck has no more cards.")
             end
             Functions.getgold(Player2)
-            print("Player "..Player2.name.." recebe 1 de gold.")
+            print("Player "..Player2.name.." receives 1 gold.")
             local i = #Player2.field
             while i > 0 do
                 Player2.field[i].stamina = 1
@@ -271,7 +271,7 @@ Functions.turn = function(t)
 
     while t == 1 or t == 2 do
         
-        print("É o turn do Player "..Player[t].name.."!")
+        print("It's player "..Player[t].name.." turn!")
         print("Life: "..Player[t].life,"Gold: "..Player[t].gold,"Magic: "..Player[t].magic,"Storm: "..Player[t].storm)
         print("1 - Hand")
         print("2 - Field")
@@ -285,14 +285,14 @@ Functions.turn = function(t)
         local option = tonumber(io.read())
 ----------SUA MÃO--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         while option == 1 do
-            print("Sua mão é:")
-            print("0 - Retornar")
+            print("Your hand:")
+            print("0 - Return")
             Functions.printzone(Player[t].hand)
             
             local opt = tonumber(io.read())
             
             if opt == nil then
-                print("SELECIONE UMA OPÇÃO VÁLIDA!")
+                print("SELECT A VALID OPTION!")
                 break
             elseif opt == 0 then
                     break
@@ -300,13 +300,13 @@ Functions.turn = function(t)
                 while opt <= #Player[t].hand and opt > 0 do
                     local card = Player[t].hand[opt]
                     Functions.printcard(card)
-                    print("0 - Retornar")
-                    print("1 - play")
+                    print("0 - Return")
+                    print("1 - Play")
                     
                     opcao = tonumber(io.read())
                     
                     if opcao == nil then
-                        print("SELECIONE UMA OPÇÃO VÁLIDA!")
+                        print("SELECT A VALID OPTION!")
                     
                     elseif opcao == 0 then
                         opt = 0
@@ -321,55 +321,57 @@ Functions.turn = function(t)
                             end
                             
                     elseif opcao == 1 and Player[t].gold < Player[t].hand[opt].cost then
-                        print("VOCÊ NÃO TEM gold SUFICIENTE! >:x")
+                        print("YOU DON'T HAVE ENOUGH GOLD! :x")
                     else
-                        print("SELECIONE UMA OPÇÃO VÁLIDA!")
+                        print("SELECT A VALID OPTION")
                     end
                 end
             else
-                print("SELECIONE UMA OPÇÃO VÁLIDA!")
+                print("SELECT A VALID OPTION!")
             end
         end
 ------------------fieldS-------------------
         while option == 2 do
-            print("Selecione o field:")
-            print("0 - Retornar")
-            print("1 - Seu field")
-            print("2 - field do oponente")
+            print("Select field:")
+            print("0 - Return")
+            print("1 - Your field")
+            print("2 - Opponent's field")
                     
             local num = tonumber(io.read())
             
             if num == nil then
-                print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                print("THAT IS NOT A VALID OPTION!")
                 break
             elseif num == 0 then
                 break
             end
                 
             while num == 1 do
-                print("Seu field:")
-                print("0 - Retornar")
+                print("Your field:")
+                print("0 - Return")
                 Functions.printzone(Player[t].field)
                 local opcao = tonumber(io.read())
                 if opcao == nil then
-                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                    print("THAT IS NOT A VALID OPTION!")
                     break
                 elseif opcao == 0 then
                     break
                 elseif opcao <= #Player[t].field then
                     local card = Player[t].field[opcao]
                     Functions.printcard(card)
-                    print("0 - Retornar")
-                    print("1 - Atacar")
+                    print("0 - Return")
+                    print("1 - Attack")
                     if card.effect.ability then
-                        print("2 - Ativar ability")
+                        print("2 - Ability")
                     end
-                    if card.effect.ultra and Player[t].ultra > 0 then
+                    if card.effect.ability and card.effect.ultra and Player[t].ultra > 0 then
                         print("3 - ULTRA")
+                    elseif card.effect.ultra and Player[t].ultra > 0 then
+                        print("2 - ULTRA")
                     end
                     local decisao = tonumber(io.read())
                     if decisao == nil then
-                        print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                        print("THAT IS NOT A VALID OPTION!")
                         break
                     elseif decisao == 0 then
                         break
@@ -377,17 +379,17 @@ Functions.turn = function(t)
                     elseif decisao == 1 then
                         while decisao == 1 do
                             local atacante = card
-                            print("Selecione o alvo:")
-                            print("0 - Retornar")
-                            print("1 - Oponente")
+                            print("Select target:")
+                            print("0 - Return")
+                            print("1 - Opponent")
                             if #Player[y].field > 0 then
-                                print("2 - Units")
+                                print("2 - Enemy units")
                             end
                                     
                             local alvo = tonumber(io.read())
                             
                             if alvo == nil then
-                                print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                                print("THAT IS NOT A VALID OPTION!")
                                 decisao = 0
                                 break
                             elseif alvo == 0 then
@@ -397,26 +399,26 @@ Functions.turn = function(t)
                             elseif alvo == 1 then
                                 if atacante.stamina > 0 then
                                     Player[y].life = Player[y].life - atacante.power
-                                    print("A life do Player "..Player[y].name.." é agora "..Player[y].life..".")
+                                    print("Player "..Player[y].name.."'s life is now "..Player[y].life..".")
                                     atacante.stamina = atacante.stamina - 1
                                     decisao = 0
                                     break
                                 else
-                                    print("ESTA Unit NÃO TEM MAIS ENERGIA PARA ATACAR! :'(")
+                                    print("THIS UNIT HAS NO ENERGY LEFT TO ATTACK! :'(")
                                     decisao = 0
                                     break
                                 end
                             end
                                     
                             while alvo == 2 and #Player[y].field > 0 do
-                                print("Selecione o alvo:")
-                                print("0 - Retornar")
+                                print("Select target:")
+                                print("0 - Return")
                                 Functions.printzone(Player[y].field)
                                 
                                 local num2 = tonumber(io.read())
                                         
                                 if num2 == nil then
-                                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                                    print("THAT IS NOT A VALID OPTION!")
                                     decisao = 0
                                     break
                                 elseif num2 == 0 then
@@ -429,14 +431,14 @@ Functions.turn = function(t)
                                     decisao = 0
                                     break
                                 else
-                                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                                    print("THAT IS NOT A VALID OPTION!")
                                     decisao = 0
                                     break
                                 end
                             end
                                     
                             if alvo ~= nil and alvo ~= 0 and alvo ~= 1 and alvo ~= 2 then
-                                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                                    print("THAT IS NOT A VALID OPTION!")
                                     break
                             end
                             decisao = 0
@@ -447,34 +449,37 @@ Functions.turn = function(t)
                         card.stamina = card.stamina-1
                         break
                     elseif decisao == 2 and card.effect.ability and card.stamina < 1 then
-                        print("ESTA Unit NÃO TEM MAIS ENERGIA!")
+                        print("THAT UNIT HAS NO ENERGY LEFT!")
                         break
+                    elseif decisao == 2 and Player[t].ultra > 0 and card.effect.ultra and card.stamina > 0 and card.effect.ability == nil then
+                        card.effect.ultra(Player[t],Player[y])
+                        Player[t].ultra = Player[t].ultra-1
                     elseif decisao == 3 and Player[t].ultra > 0 and card.effect.ultra and card.stamina > 0 then
                         card.effect.ultra(Player[t],Player[y])
                         Player[t].ultra = Player[t].ultra-1
                     else
-                        print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                        print("THAT IS NOT A VALID OPTION!")
                         break
                     end
                 else
-                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                    print("THAT IS NOT A VALID OPTION!")
                     break
                 end
             end
                 
             while num == 2 do
-                print("field do oponente:")
-                print("0 - Retornar")
+                print("Opponent's field:")
+                print("0 - Return")
                 Functions.printzone(Player[y].field)
                 local opcao = tonumber(io.read())
                 if opcao == nil then
-                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                    print("THAT IS NOT A VALID OPTION!")
                     break
                 elseif opcao == 0 then
                     break
                 elseif opcao <= #Player[y].field then
                     Functions.printcard(Player[y].field[opcao])
-                    print("Qualquer comando - Retornar")
+                    print("[Any command] - Return")
                     local option = tonumber(io.read())
                     if option then
                         break
@@ -483,52 +488,52 @@ Functions.turn = function(t)
             end
 
             if num ~= nil and num ~= 0 and num ~= 1 and num ~= 2 then
-                print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                print("THAT IS NOT A VALID OPTION!")
                 break
             end
         end
         ------------------room--------------------------------------------
         while option == 3 do
-            print("Sua room:")
-            print("0 - Retornar")
+            print("Your room:")
+            print("0 - Return")
             Functions.printzone(Player[t].room)
             local opcao = tonumber(io.read())
             if opcao == nil then
-                print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                print("THAT IS NOT A VALID OPTION!")
                     break
             elseif opcao == 0 then
                     break
             elseif opcao <= #Player[t].room then
                 local card = Player[t].room[opcao]
                 Functions.printcard(card)
-                print("0 - Retornar")
+                print("0 - Return")
                 if card.stamina > 0 then
-                    print("1 - Ativar ability")
+                    print("1 - Ability")
                 end
                 local decisao = tonumber(io.read())
                 if decisao == nil then
-                    print("ESSA NÃO É UMA OPÇÃO VÁLIDA!")
+                    print("THAT IS NOT A VALID OPTION!")
                     break
                 elseif decisao == 0 then
                     break
                 elseif decisao == 1 and card.stamina > 0 then
                     card.effect.ability(Player[t],Player[y])
-                    card.lealdade = card.lealdade-1
+                    card.loyalty = card.loyalty-1
                     card.stamina = card.stamina-1
-                    if card.lealdade < 1 then
+                    if card.loyalty < 1 then
                         Functions.destroy(card,Player1,Player[t],Player[y])
                     end
                 else
-                    print("SELECIONE UMA OPÇÃO VÁLIDA!")
+                    print("SELECT A VALID OPTION!")
                 end
             else
-                print("SELECIONE UMA OPÇÃO VÁLIDA!")
+                print("SELECT A VALID OPTION!!")
             end
         end
         
         while option == 4 do
-            print("Seu cemitério é:")
-            print("0 - Retornar")
+            print("Your graveyard:")
+            print("0 - Return")
             Functions.printzone(Player[t].graveyard)
             
             local opti = tonumber(io.read())
@@ -536,7 +541,7 @@ Functions.turn = function(t)
             if opti == 0 then
                 break
             else
-                print("Você não pode fazer nada por eles :'(")
+                print("You can't do anything for them. :'(")
             end
         end
         
@@ -546,7 +551,7 @@ Functions.turn = function(t)
         end
         
         while #Player[t].extra > 0 and option == 6 do
-            print("Your extra deck is:")
+            print("Your extra deck:")
             print("0 - Return")
             Functions.printzone(Player[t].extra)
             local option = tonumber(io.read())
@@ -587,12 +592,12 @@ Functions.turn = function(t)
         end
             
         if option ~= 1 and option ~= 2 and option ~= 3 and option~= 4 and option ~= 5 and option ~= 6 then
-            print("Selecione uma opção válida!")
+            print("SELECT A VALID OPTION!")
         end
     end
 end
-------------JOGO----------------
-Functions.jogo = function()
+------------GAME----------------
+Functions.game = function()
 
     Functions.shuffle2(Player[1].deck)
     Functions.shuffle2(Player[2].deck)
@@ -612,19 +617,19 @@ Functions.jogo = function()
         t = 1  y = 2
         Functions.turn(t)
         if Player[1].life <1 then
-            print(Player[2].name.." venceu o jogo!")
+            print(Player[2].name.." won!")
             break
         elseif Player[2].life <1 then
-            print(Player[1].name.." venceu o jogo!")
+            print(Player[1].name.." won!")
             break
         end
         t = 2  y = 1
         Functions.turn(t)
         if Player[1].life <1 then
-            print(Player[2].name.." venceu o jogo!")
+            print(Player[2].name.." won!")
             break
         elseif Player[2].life <1 then
-            print(Player[1].name.." venceu o jogo!")
+            print(Player[1].name.." won!")
         end
     end
 end
