8da58f6719aef42ec4cdee48c4e20f18c321c72a
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-06-15 19:29:31 -0300

    Create Player.lua

---
+++ Player.lua
@@ -0,0 +1,46 @@
+local Player = {}
+
+
+Player[1] = {
+    life = 100,
+    gold = 0,
+    magic = 1,
+    ultra = 1,
+    level = 1,
+    deck = {},
+    extra = {},
+    hand = {},
+    field = {},
+    graveyard = {},
+    exile = {},
+    storm = 0,
+    lastsupport = {},
+    room = {},
+    temple = {},
+    turnevents = {lifegained = 0,lifelost = 0,cardsdrawn = 0,cardsdiscarded = 0,goldreceived = 0,goldspent = 0,cardsplayed = 0,unitssummoned = 0,supportscast = 0,spellscast = 0,alliescalled = 0,totemsbuilt = 0,
+        damagedealt = 0,attackingunits = 0,destroyedunits = 0
+    }
+}
+
+Player[2] = {
+    life = 100,
+    gold = 0,
+    magic = 1,
+    ultra = 1,
+    level = 1,
+    deck = {},
+    extra = {},
+    hand = {},
+    field = {},
+    graveyard = {},
+    exile = {},
+    storm = 0,
+    lastsupport = {},
+    room = {},
+    temple = {},
+    turnevents = {lifegained = 0,lifelost = 0,cardsdrawn = 0,cardsdiscarded = 0,goldreceived = 0,goldspent = 0,cardsplayed = 0,unitssummoned = 0,supportscast = 0,spellscast = 0,alliescalled = 0,totemsbuilt = 0,
+        damagedealt = 0,attackingunits = 0,destroyedunits = 0
+    }
+}
+
+return Player
