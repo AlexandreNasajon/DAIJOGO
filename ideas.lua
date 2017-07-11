aef40d707a50c868d8a0e3fc52f7ef620132ed7a
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-07-10 22:48:14 -0300

    Create ideas.lua

---
+++ ideas.lua
@@ -0,0 +1,164 @@
+"Main Deck"
+
+"Ally"---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        "Loyal Blinker" -- c:0/loyalty: 1/+1: exile a unit you control/-1: summon an exiled unit
+        "Cube Guardian" -- heroic/c:0/loyalty: 1/+1: add top card to cube/-1: switch card from hand with a cube card
+        "Spell Smith" -- c:0/loyalty: 3/+2: reveal top card, if it is spell add to hand/-3: enhance a spell in your hand
+        "Moonlight Owl" -- c:0/loyalty: 1/+1: add a spell from graveyard to bottom/0: scry 2
+        "Secluded Necromancer" -- c:0/loyalty: 2/+1: sacrifice a unit/-1: zombify a unit in your graveyard then summon it
+        
+"Spell"-------
+        
+        "Enhancing Spell" -- you may enhance a spell in your hand and/or a unit gains 3 xp
+        "Into the Nexus" -- look at top two cards of deck, play one, other to bottom
+        "Journey's Climax" -- activate the beast mode of a unit or ally
+        "Chant to the Forgotten" -- sacrifice a unit to summon one of the seven forgotten
+            "Andos, " -- if there are no units attached to this, you may attach an enemy unit to this/this gains power equal to the attached unit's power
+            "" -- 
+            ""
+            ""
+            ""
+            ""
+            ""
+            
+        "Odd Ones Ritual" -- sacrifice a unit with power 1,3,5,7,9,11,13,15,17,19 or 21 then summon the respective Odd One
+            "" -- power: 1/each player sacrifices until has 1 unit
+            "" -- power: 3/each player discards hand
+            "" -- power: 5/each player's life becomes 121
+            "" -- power: 7/each player receives 3 gold
+            "" -- power: 9/each player draws until 5 in hand
+            "" -- power: 11/
+            "" -- power: 13/
+            "" -- power: 15/
+            "" -- power: 17/
+            "" -- power: 19/
+            "" -- power: 21/
+            
+        "" -- is exiled after the effect
+
+"Unit"------------------------------------------
+        
+        "Vyx, the Necromancer" -- hero/cost:2/power: 14/if destroys, zombify then summon the destroyed unit
+            
+        "[INPUT]" -- escolhe nome antes da partida/arquétipo de herói de rpg
+        
+        "Pikachu" -- 3 herois com nomes iguais, mas efeitos diferentes
+        "Pikachu"
+        "Pikachu"
+        
+        "" -- pode sacrificar essa unit pra dar todos os efeitos dela pra uma outra, inclusive este
+        
+        "" -- herói/quando uma unidade inimiga atacar, aparece a opcao de banir esta do cemiterio e negar o ataque
+        
+"Special Deck"
+
+"God"-----------------------------------------------------------------------------------------------------------------------------------------------------------
+        "God of the Dead"
+        "God of Wisdom"
+        "God of the Nexus"
+        "God of Growth"
+        "God of Wealth"
+
+"Shadow"--------------------------------------------------------------------------------------------------------------------------------------------------------
+             -- quando é invocada, a original fica com shadow = 0
+
+        "Shadow CARDNAME" -- tem poder 0, mas tem um efeito igual ao de CARDNAME, exceto algumas
+
+
+"Beast Mode"----------------------------------------------------------------------------------------------------------------------------------------------------
+        "Imperial Breno" -- must pay 2 gold to Breno/if beast mode activated, draw a card for each unit you control and they lose 1 power
+        "Tom Unbound" -- Tom, the Chosen/you may play any number of spells each turn/if summoned for first time, you gain the technique "Magic Unleashed"/if would die, instead loses beast mode and add to extra
+        "Nyn, Nexus Heart" -- Nyn, Nexus Master must have rage 3 or more/cost: 0/if damages or destroys, you may tutor then play any card/if would die, instead loses beast mode and add to hand
+        "Eclipse Owl" -- Newmoon Owl + there must be no spells in deck/0: shuffle all spells from hand and graveyard into deck, then add 3 random spells from deck to hand and enhance them
+        ""
+        ""
+
+"Ex"------------------------------------------------------------------------------------------------------------------------------------------------------------
+        "Bloodwoods Hydra" -- Devourer Hydra must have power 28 or higher/can attack an additional time each turn
+        "Catacomb Dragon" -- Devourer Dragon must have 3 xp or more/ability: destroy all weaker enemy units
+        "Twin Peak Hunter" -- Twin Peak Roc must have 3 xp or more/once per turn, you may destroy an enemy unit
+        "Arcana Archaeologist" -- Arcana Scrolls + Archaeologist/ability: add a spell from graveyard to hand
+        "Tom, the Chosen" -- Tom, the Enchanter must have 3 xp or more/you may play an additional spell each turn/ability: search hand, deck or graveyard for a spell, enhance it then add it to hand/if would die, instead add to extra/can only be summoned by offering 3 spells from anywhere
+        "Ex Returned Witch" -- Returned Witch must have power 17 or higher/ateot if in graveyard, summon it
+        "Nyn, Nexus Master" -- Nyn, Nexus Chosen must have 3 xp or more/cost: 0/if damages or destroys, scry 2 then you may play top card/if would die, instead add to top deck and this gains 1 rage
+        "Wanderer Wizard" -- Pilgrim Mage + must have 10 or more cards in graveyard/cost: 1/ability: draw 3 discard 1
+        "Cube Lord" -- Cube Guardian + the cube must have 5 or more cards/+1: add card from top and/or from graveyard to cube/-1: add card from cube to hand
+        "Newmoon Owl" -- Moonlight Owl must have loyalty 4 or higher/+1: scry 4/-3: reveal the top 3, add all spells to hand and rest to bottom
+        "Vyx, Death Sovereign" -- Vyx, the Lich King must have 7 xp or more/ability: destroy an enemy unit, zombify it then summon it/ateot zombify then summon all units in your graveyard/cannot die
+        "Kaplan Sensei" -- Kaplan + level 3 or higher/
+        "Kaplan, Selfless One" -- Kaplan Sensei + level 6 or higher/
+        "One with All" -- Kaplan, Selfless One + level 9 or higher/technique/
+
+"Zombie Unit"--------------------------------------------------------------------------------------------------------------------------------------------------------
+             -- precisa de um efeito de zombify e reanimate ou só de reanimate se já for zumbi
+
+        "Zombie CARDNAME" -- Tem poder igual ao de CARDNAME, mas não tem nenhuma habilidade, exceto algumas que precisam estar no deck especial
+        
+        "Vyx, the Lich King" -- power: 18/if destroys, zombify then summon the destroyed unit +1 xp/ability: zombify and summon a unit in any graveyard +1 xp/ateot, if in graveyard, summon this
+        
+
+"Devourer"------------------------------------------------------------------------------------------------------------------------------------------------------
+        "Cave Dragon" -- can only be summoned by offering 3 units/if destroys, gains 1 xp
+        "Devourer of Shadows" -- can only be summoned by offering 2 shadow units/gains power equal to the units' total power/if would die, instead loses 5 power, then if power < 1 dies
+        "Redwoods Hydra" -- can only be summoned by offering 1 or more units/gains 7 power for each offered unit/if destroys, gains 7 power
+        "Uncaged Djinn" -- can only be summoned by offering 1 unit/if summoned, draw a card and lose 6 life/at end of turn, offer another unit and draw a card or lose 18 life and draw a card
+        "Dusk Spirit" -- can only be summoned by offering 3 units/at end of turn, exile an enemy unit
+        "Totem Enchanter" -- can only be summoned by offering 3 totems/at end of turn, build a heroic totem from your special deck
+        "Twin Peak Roc" -- can only be summoned by offering 2 units/ability: destroy a weaker unit then gains 1 xp
+        "Tom, the Enchanter" -- hero/can only be summoned by offering 2 spells from your graveyard and paying 1 magic/ability: tutor a spell and enhance it then gains 1 xp/if would die, instead add to extra
+
+"Enhanced Spell"---------------------------------------------------------------------------------------------------------------------------------------------------
+                 -- precisa de um efeito de enhance
+
+        "Guided Recover" -- Past Recover/add up to three cards from graveyard to hand
+        "The Facades" -- The Faces/may copy the last spell your opponent played and may copy the last spell you played
+        "Collect Soul" -- Raise Dead/zombify any unit in any graveyard then summon it
+        "Army of the Dead" -- Raise Dead/zombify all units from your graveyard then summon them
+        "Zombie Apocalypse" -- Army of the Dead or Collect Soul/zombify all units in all graveyards then summon them
+        "Purge Darkness" -- Eliminate Evil or Lay Waste/destroy all enemy units
+        "Unleash Magic" -- Enhancing Spell/you gain the technique "Magic Unleashed"
+        "Master Transmutation" -- Transmutation/select two or more units on your field and/or hand and/or deck and/or graveyard, transmute them then summon it
+        "Shadow Embrace" -- Shadow Pact/sacrifice all your non-shadow units then summon their shadows
+        "Double Disgrace" -- Shadow Pact or Raise Dead/Sacrifice a unit, zombify it then summon it and its shadow
+        "Visions from Beyond" -- Enlightenment/draw three cards
+        "The Revelation" -- Visions from Beyond/draw four cards
+        "Omniscience" -- The Revelation/draw your deck
+        "Nexus' Core" -- Into the Nexus/reveal the top four cards of your deck, you may play two of them, then put the rest on the bottom
+        "Angelic Grace" -- Graceful Grace/draw five then discard two
+        "Sojourner's Portal" -- Unit Seeking/tutor a unit then summon it
+        "Beyond the Threshold" -- Journey's Climax/activate the beast mode of a unit or ally permanently
+
+"Chimera"-------------------------------------------------------------------------------------------------------------------------------------------------------
+              -- units transmutadas/não precisa de nenhuma carta, mas alguns efeitos podem ajudar a fazer sem precisar ter as duas units no campo
+
+        "Growing Replicator" -- Replicator Ooze + Growing Slime/ateot doubles power then summons copy
+        "Thousand Replicator" -- Replicator Ooze + Thousand Faces/ateot summons a copy of any unit
+        ""
+        ""
+        ""
+        ""
+"Heroic Totem"---------------------------------------------------------------------------------------------------------------------------------------------------------
+              -- só pode ter um de cada, tipo heróis
+
+        "Spell Enhancer" -- ateot enhance a spell in your hand or deck
+        "Chimera Heart" -- ateot transmute two or more units in your hand or deck
+        "Zombifier Totem" -- ateot zombify a unit in your graveyard then summon it
+        "Shadow Gate" -- ateot sacrifice a unit then summon its shadow
+        "Architect's Rod" -- ateot mill top 5 then shuffle up to 5
+        "Nexus Gate" -- ateot you may play the top card of your deck
+        "Lucky Charm" -- ateot receive 1 gold
+        
+        
+"Techniques"-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        "Magic Unleashed" -- if you play a spell, you may instead enhance it then play it
+
+"Spell"----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        
+        "Unlock the Cube" -- is immediately played when the first letter of the cards in the cube spell in order the name of one of the gods/eles mudam as regras do jogo drasticamente
+            "Ghrtf" -- 
+            "Yjklx"
+            "Pvzcq"
+            "Euswd"
